6dc154b8c61b44644076f4002d6bab905f0659c6
omale-godwin <godwinpeace3@gmail.com>
2023-09-18 12:36:57 +0100

    task 2

--- console.py
+++ console.py
@@ -126,6 +126,42 @@ class HBNBCommand(cmd.Cmd):
         print(new_instance.id)
         storage.save()
 
+
+    def do_create(self, line):
+            """Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
+            Create a new class instance with given keys/val and print its id.
+            """
+            try:
+                if not line:
+                    raise SyntaxError()
+                my_list = line.split(" ")
+
+                kwargs = {}
+                for i in range(1, len(my_list)):
+                    key, value = tuple(my_list[i].split("="))
+                    if value[0] == '"':
+                        value = value.strip('"').replace("_", " ")
+                    else:
+                        try:
+                            value = eval(value)
+                        except (SyntaxError, NameError):
+                            continue
+                    kwargs[key] = value
+
+                if kwargs == {}:
+                    obj = eval(my_list[0])()
+                else:
+                    obj = eval(my_list[0])(**kwargs)
+                    storage.new(obj)
+                    storage.save()
+                print(obj.id)
+
+            except SyntaxError:
+                print("** class name missing **")
+            except NameError:
+                print("** class doesn't exist **")
+
+
     def help_create(self):
         """ Help information for the create method """
         print("Creates a class of any type")
