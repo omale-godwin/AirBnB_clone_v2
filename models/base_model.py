d1ac92d6340ca8818153f7b2eca8bdd34ec6a973
JustusOmbok <ombokjustus@gmail.com>
2023-09-20 20:17:23 +0300

    update dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -18,11 +18,10 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.utcnow()
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.utcnow()
 
-        else:
+        if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
