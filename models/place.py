b4a491daa74428be3122b1bb136cd2cc0a41da57
JustusOmbok <ombokjustus@gmail.com>
2023-09-20 16:48:34 +0300

    update place

--- models/place.py
+++ models/place.py
@@ -11,7 +11,7 @@ metadata = BaseModel.metadata
 place_amenity = Table(
         'place_amenity',
         metadata,
-        Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False)
+        Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
         Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
         )
 
