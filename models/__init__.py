0171399ba0ac5629a5274dd7f279fdbabb2510b6
JustusOmbok <ombokjustus@gmail.com>
2023-09-20 18:01:43 +0300

    update place

--- models/__init__.py
+++ models/__init__.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
 """Specifies which storage type to be used"""
 
-import os
+from os import getenv
 
-storage_type = os.getenv('HBNB_TYPE_STORAGE')
+from models.base_model import BaseModel
+from models.base_model import Base
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
-if storage_type == 'db':
+
+if getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
+    storage.reload()
+
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-
-
-storage.reload()
+    storage.reload()
