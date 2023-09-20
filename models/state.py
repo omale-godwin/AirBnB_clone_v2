6105b377a3307f1821665119f80008f50aecda16
JustusOmbok <ombokjustus@gmail.com>
2023-09-20 17:14:36 +0300

    update place

--- models/state.py
+++ models/state.py
@@ -10,7 +10,7 @@ import models
 class State(BaseModel, Base):
     """ State class """
 
-    __tablename__ == 'states'
+    __tablename__ = 'states'
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
