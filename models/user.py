1e2526cdcbbc79b7dd22ac9feb42b2ca987a565a
JustusOmbok <ombokjustus@gmail.com>
2023-09-20 18:22:43 +0300

    place update

--- models/user.py
+++ models/user.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from model.review import Review
+from models.review import Review
 
 
 class User(BaseModel, Base):
