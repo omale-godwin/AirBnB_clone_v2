b827d8b1a8eeb6d7bd675163d13811e597d6db4c
JustusOmbok <ombokjustus@gmail.com>
2023-09-20 16:57:57 +0300

    update place

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models.place import PlaceAmenity
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
@@ -12,4 +11,4 @@ class Amenity(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
 
-    place_amenities = relationship('PlaceAmenity', backref='amenity', cascade='all, delete')
+
