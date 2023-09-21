f26a1f3d50aa39b13569508ed63ee13aed64d062
JustusOmbok <ombokjustus@gmail.com>
2023-09-21 11:09:34 +0300

    update city

--- models/city.py
+++ models/city.py
@@ -14,5 +14,5 @@ class City(BaseModel, Base):
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
 
-    places = relationship('Place', backref='cities')
+    places = relationship('Place', backref='city')
     state_rel = relationship('State', backref='cities')
