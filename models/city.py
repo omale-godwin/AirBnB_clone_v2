4261b4f652a39ce0f5903a1a369185b5bbd47cee
JustusOmbok <ombokjustus@gmail.com>
2023-09-21 11:39:04 +0300

    update city

--- models/city.py
+++ models/city.py
@@ -3,7 +3,7 @@
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
-from models
+import models
 
 
 class City(BaseModel, Base):
